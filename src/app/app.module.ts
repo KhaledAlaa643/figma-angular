9714bf2b0a36b509520ceabd75d5a90b12315615
KhaledAlaa643 <122280211+KhaledAlaa643@users.noreply.github.com>
2023-11-28 15:29:06 +0200

    upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { ChartModule } from 'primeng/chart';
+import { TableModule } from 'primeng/table'; 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxPaginationModule,
+    NgScrollbarModule,
+    ChartModule,
+    TableModule,
+    NgbModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
